4bebb98a99c7feb38bfe347add3c9a22e6af79b1
Ethan <ethan@CS.local>
2024-08-15 15:25:11 -0400

    changes

---
+++ Drawing1.dwl
@@ -0,0 +1,3 @@
+ethan
+NEWDELL 
+August 15, 2024  1:09:53 PM
--- Program.cs
+++ Program.cs
@@ -1,47 +1,68 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-
 using FlaUI.Core;
 using FlaUI.UIA3;
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Conditions;
 using FlaUI.Core.Input;
+using System.CodeDom;
+using System.Drawing;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Enter a order number: ");
-        string? orderNumber = Console.ReadLine();
+        // Console.Write("Enter a order number: ");
+        // string? orderNumber = Console.ReadLine();
 
-        while (!CheckOrderExists(orderNumber))
+        // while (!CheckOrderExists(orderNumber))
+        // {
+        //     Console.Write("Invalid order number. Enter a valid order number: ");
+        //     orderNumber = Console.ReadLine();
+        // }
+
+        UIA3Automation? automation = new UIA3Automation();
+        Application app = Application.Launch(@"c:\Program Files\Microvellum\Toolbox OEM 2022\toolbox.exe");
+        ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+        Window? window = null;
+        Button? continueBtn = null;
+
+        while (true)
         {
-            Console.Write("Invalid order number. Enter a valid order number: ");
-            orderNumber = Console.ReadLine();
-        }
+            window = app.GetMainWindow(automation);
+            continueBtn = window.FindFirstDescendant(cf.ByAutomationId("btnStartApplication")).AsButton();
 
-        using (var automation = new UIA3Automation())
-        {
-            Application app = Application.Launch(@"c:\Program Files\Microvellum\Toolbox OEM 2022\toolbox.exe");
-            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
-            Thread.Sleep(500);
-            var window = app.GetMainWindow(automation);
-
-            while (true)
+            if (continueBtn != null)
             {
-                window = app.GetMainWindow(automation);
-                var continueBtn = window.FindFirstDescendant(cf.ByAutomationId("btnStartApplication")).AsButton();
-
-                if (continueBtn != null)
-                {
-                    continueBtn.Invoke();
-                    break;
-                }
-
-                Thread.Sleep(500);
+                continueBtn.Invoke();
+                break;
             }
+
+            Thread.Sleep(500);
         }
+
+        Menu? menu = null;
+
+        while (true)
+        {
+            window = app.GetMainWindow(automation);
+            menu = window.FindFirstDescendant(cf.ByName("MenuBar1")).AsMenu();
+
+            if (menu != null && menu.BoundingRectangle.Top > 200)
+            {
+                var menuRect = menu.BoundingRectangle;
+                var mousePos = Mouse.Position;
+                Mouse.Position = new Point(menuRect.Left + 150, menuRect.Top + 10);
+                Mouse.Click();
+                Mouse.Position = mousePos;
+                break;
+            }
+
+            Thread.Sleep(500);
+        }
+
     }
 
     private static bool CheckOrderExists(string? orderNumber)
